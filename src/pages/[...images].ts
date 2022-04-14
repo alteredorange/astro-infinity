b47cab5b389b8527183cca0be3360049d922fe17
Kyle <alteredorange@gmail.com>
2022-04-14 07:35:14 -0600

    feat: Add Blur and height controls

--- src/pages/[...images].ts
+++ src/pages/[...images].ts
@@ -1,7 +1,21 @@
 import sharp from 'sharp';
-
+// const { SSR, IMAGE_API_SECRET, PUBLIC_SOME_KEY } = import.meta.env;
 
 export async function get(params, request) {
+	// console.log(request)
+	const acceptHeaders = request.headers.get('accept');
+	const format = acceptHeaders == "*/*" ? "avif" : acceptHeaders.includes("image/avif") ? "avif" : acceptHeaders.includes("image/webp") ? "webp" : "jpg";
+	console.log({format})
+	const CHdpr = request.headers.get('DPR')
+	const CHwidth = request.headers.get('Viewport-Width') || Math.round(request.headers.get('Width') * (1/CHdpr)) || 0;
+console.log({CHdpr})
+	const test = request.headers.get('Viewport-Width') || Math.round(request.headers.get('Width') * (1/CHdpr)) || 0;
+	console.log({test})
+
+// 	const h = request.headers.get('height')
+// console.log({w})
+// const sec = request.headers.get('Sec-CH-Width')
+// console.log({sec})
 
 	// const img = await fetch ("https://picsum.photos/500/400?random");
 	// const arrayBuffer = await img.arrayBuffer();
@@ -29,9 +43,9 @@ export async function get(params, request) {
 
 	const searchParams = new URL(request.url).searchParams //gets the params
 	const src = searchParams.get('src') //gets the src
-	const width = Number(searchParams.get('width')) || null//gets the width
-	const rotate = Number(searchParams.get('rotate')) || null //gets the rotatation angle
-	const height = Number(searchParams.get('height')) //gets the height
+	const width = Number(searchParams.get('width')) || Number(CHwidth) || undefined//gets the width
+	const rotate = Number(searchParams.get('rotate')) || undefined //gets the rotatation angle
+	const height = Number(searchParams.get('height')) || undefined//gets the height
 	const grayscale = searchParams.get('grayscale') //gets the grayscale
 	const blur = searchParams.get('blur') //gets the blur
 	const sharpen = searchParams.get('sharpen') //gets the sharpen
@@ -42,22 +56,46 @@ export async function get(params, request) {
 	const imgData = src.startsWith('/assets/') ? 'public' + src : await fetch(src).then(res => res.buffer())
 
 
-
+if (blur) {
 	//sharp needs path from root directory
 	const image = await sharp(imgData )
 	.rotate(rotate)
-	.resize(width)
-	.webp()
+	.resize({width: 20, height: 20, fit: 'cover'})
+	.toFormat(format)
+	// .toColourspace('b-w')
+	// .jpeg({ mozjpeg: true, quality: 10, progressive: false })
+	// .toFormat(format)
 	.toBuffer()
 
 
 	return new Response(image, {
 		headers: {
-		  'Content-Type': 'image/webp',
-		  'Cach-Control': 'public, max-age=31536000',
-		  'mime-type': 'image/webp'
+		  'Content-Type': 'image/' + format,
+		  'Cach-Control': 'public, max-age=15552000',
+		  'mime-type': 'image/webp',
+		  'Vary': 'Width, DPR, Viewport-Width'
+
 		}
 	      });
+	} else {
+
+//sharp needs path from root directory
+const image = await sharp(imgData )
+.rotate(rotate)
+.resize(width, 500, {fit: 'cover', position: 'attention'})
+.toFormat(format)
+.toBuffer()
+
+
+return new Response(image, {
+	headers: {
+	  'Content-Type': 'image/' + format,
+	  'Cach-Control': 'public, max-age=15552000',
+	  'mime-type': 'image/webp'
+	}
+      });
+
+	}
 
 
 
