33453022e6eb6c48c2d24ac83e45eeb3d740cc06
Kyle <alteredorange@gmail.com>
2022-04-29 10:52:50 -0600

    refactor: demo cleanup

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,22 +1,10 @@
 import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import partytown from '@astrojs/partytown'
 import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind' // import astroImagePlugin from 'astro-imagetools/plugin'
 
 // import vercel from '@astrojs/vercel';
 
-import netlify from '@astrojs/netlify/functions'
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), partytown(), tailwind(), node()],
-  vite: {
-    vite: {
-      // plugins: [astroImagePlugin]
-    },
-    ssr: {
-      external: ['svgo', 'astro-icon', 'sharp']
-    }
-  }
+  integrations: [tailwind(), node()]
 })
